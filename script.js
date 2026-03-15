654645a974313134d827004afe0191818c75bd83
SujalPaul <98580092+SujalPaul@users.noreply.github.com>
2026-03-15 13:34:20 +0530

    Refactor nutrition analysis function to use image input

--- script.js
+++ script.js
@@ -1,109 +1,37 @@
-async function getNutrition(){
-
-let food = document.getElementById("foodInput").value;
-
-let response = await fetch(
-
-"https://trackapi.nutritionix.com/v2/natural/nutrients",
-
-{
-
-method:"POST",
-
-headers:{
-
-"x-app-id":NUTRITION_APP_ID,
-
-"x-app-key":NUTRITION_API_KEY,
-
-"Content-Type":"application/json"
-
-},
-
-body: JSON.stringify({
-
-query: food
-
-})
-
-}
-
-);
-
-let data = await response.json();
-
-let item = data.foods[0];
-
-document.getElementById("result").innerHTML =
-
-`
-Food: ${item.food_name} <br>
-Calories: ${item.nf_calories} kcal <br>
-Protein: ${item.nf_protein} g <br>
-Carbs: ${item.nf_total_carbohydrate} g <br>
-Fat: ${item.nf_total_fat} g
-`;
-
-}
-
 async function analyzeImage(){
 
-let file = document.getElementById("imageInput").files[0];
+const fileInput = document.getElementById("imageInput");
 
-let reader = new FileReader();
+const file = fileInput.files[0];
 
-reader.onloadend = async function(){
+if(!file){
 
-let base64 = reader.result.split(",")[1];
+alert("Please upload an image");
 
-let response = await fetch(
+return;
 
-"https://api.clarifai.com/v2/models/food-item-recognition/outputs",
+}
+
+const formData = new FormData();
+
+formData.append("image", file);
+
+const response = await fetch(
+
+"http://localhost:3000/analyze-food",
 
 {
 
 method:"POST",
 
-headers:{
-
-"Authorization":"Key " + CLARIFAI_API_KEY,
-
-"Content-Type":"application/json"
-
-},
-
-body: JSON.stringify({
-
-inputs:[{
-
-data:{
-
-image:{
-
-base64:base64
-
-}
-
-}
-
-}]
-
-})
+body:formData
 
 }
 
 );
 
-let data = await response.json();
+const data = await response.json();
 
-let detectedFood = data.outputs[0].data.concepts[0].name;
-
-document.getElementById("foodInput").value = detectedFood;
-
-getNutrition();
-
-};
-
-reader.readAsDataURL(file);
+document.getElementById("result").innerText = data.result;
 
 }
