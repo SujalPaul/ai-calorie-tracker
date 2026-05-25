137bfa3d07ff9f46a0711266c0e9f716e82df7be
SujalPaul <98580092+SujalPaul@users.noreply.github.com>
2026-05-25 21:12:59 +0530

    Update analyze-food.js

--- api/analyze-food.js
+++ api/analyze-food.js
@@ -66,11 +66,9 @@ export default async function handler(req, res) {
             ];
         }
 
-        const response =
-await fetch(
+       const response = await fetch(
 
-`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${GEMINI_API_KEY}`,
-
+`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${GEMINI_API_KEY}`,
             {
 
                 method: "POST",
